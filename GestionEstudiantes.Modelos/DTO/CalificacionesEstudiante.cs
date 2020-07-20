3e308e90b70229b92e328f5ee1e756bbd9de6912
Luis Felipe Diaz Valbuena <luis.felipe.diaz.v@gmail.com>
2020-07-20 11:51:23 -0500

    Ajuste en el constructor del dto

--- GestionEstudiantes.Modelos/DTO/CalificacionesEstudiante.cs
+++ GestionEstudiantes.Modelos/DTO/CalificacionesEstudiante.cs
@@ -6,16 +6,16 @@ namespace GestionEstudiantes.Modelos.DTO
 {
     public class CalificacionesEstudiante
     {
-        public string TarjetaIdentidad { get; private set; }
         public int IdGrupo { get; private set; }
+        public string TarjetaIdentidad { get; private set; }
         public float? CalificacionPrimerPeriodo { get; private set; }
         public float? CalificacionSegundoPeriodo { get; private set; }
         public float? CalificacionTercerPeriodo { get; private set; }
 
-        public CalificacionesEstudiante(string tarjetaIdentidad, int idGrupo, float? calificacionPrimerPeriodo, float? calificacionSegundoPeriodo, float? calificacionTercerPeriodo)
+        public CalificacionesEstudiante(int idGrupo, string tarjetaIdentidad, float? calificacionPrimerPeriodo, float? calificacionSegundoPeriodo, float? calificacionTercerPeriodo)
         {
-            TarjetaIdentidad = tarjetaIdentidad;
             IdGrupo = idGrupo;
+            TarjetaIdentidad = tarjetaIdentidad;
             CalificacionPrimerPeriodo = calificacionPrimerPeriodo;
             CalificacionSegundoPeriodo = calificacionSegundoPeriodo;
             CalificacionTercerPeriodo = calificacionTercerPeriodo;
